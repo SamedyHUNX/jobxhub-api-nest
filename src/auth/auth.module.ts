218669070d24082791ef4b1db398164e96bb4615
samedyhunx <samedy.hunx@gmail.com>
2026-01-16 17:33:25 +0700

    fixed as suggested

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,25 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { S3Module } from '@/s3/s3.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
-  imports: [S3Module, PassportModule],
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [
+    S3Module,
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: Number(configService.get<string>('JWT_EXPIRES_IN')),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController, JwtStrategy],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
