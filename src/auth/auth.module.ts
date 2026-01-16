a96e7ac17a7cc5bc0e24383774836863004db84a
samedyhunx <samedy.hunx@gmail.com>
2026-01-16 16:57:45 +0700

    auth passport installation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { S3Service } from '@/s3/s3.service';
+import { PassportModule } from '@nestjs/passport';
+import { S3Module } from '@/s3/s3.module';
 
 @Module({
+  imports: [S3Module, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService, S3Service],
+  providers: [AuthService],
 })
 export class AuthModule {}
