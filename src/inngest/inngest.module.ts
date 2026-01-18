05fb70c80fd287a8eea3a2a9aa57b4d82f2464a4
samedyhunx <samedy.hunx@gmail.com>
2026-01-18 15:12:28 +0700

    removed the import issue

--- src/inngest/inngest.module.ts
+++ src/inngest/inngest.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { InngestService } from './inngest.service';
 import { InngestController } from './inngest.controller';
 
 @Module({
   controllers: [InngestController],
-  providers: [InngestService],
 })
 export class InngestModule {}
