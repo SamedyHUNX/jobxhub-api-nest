69c97f5dabc75786e7aaebbd3967d7ba17fb229d
samedyhunx <samedy.hunx@gmail.com>
2026-01-19 19:20:41 +0700

    redis setup module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Headers,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { SignUpDto } from './dtos/auth.dto';
+import { ImageValidationPipe } from '@/utils/image-validation-pipe';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  @UseInterceptors(FileInterceptor('image'))
+  signUp(
+    @Body() data: SignUpDto,
+    @UploadedFile(new ImageValidationPipe()) file: Express.Multer.File,
+    @Headers('accept-language') acceptLanguage: string,
+  ) {
+    return this.authService.signUp(data, file, acceptLanguage);
+  }
 }
