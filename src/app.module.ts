b1b78eb23c0d11e3c39d91e3a1804a39294d1541
samedyhunx <samedy.hunx@gmail.com>
2026-01-19 19:29:08 +0700

    fixed as suggested

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { S3Module } from './s3/s3.module';
 import { InngestModule } from './inngest/inngest.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [AuthModule, S3Module, InngestModule],
+  imports: [AuthModule, S3Module, InngestModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
